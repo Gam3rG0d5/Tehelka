c35911943c6f40b19a9a0630072bf585030f6b6a
Gam3rG0d5 <137208903+Gam3rG0d5@users.noreply.github.com>
2025-05-13 21:49:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   host: 'BOOOOOM69.aternos.me',
   port: 57404,
   username: 'Tehelka',
-  version: false, // Auto-detect to fix PartialReadError
+  version: false, // Auto-detect to handle ViaBackwards and fix PartialReadError
   authmePassword: process.env.AUTHME_PASSWORD || 'Tehelka123' // Fallback for testing
 }
